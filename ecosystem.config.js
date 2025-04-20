0bc47514a4eda6980aa4a8ae14f528f9fb585340
Mart510 <martynandrewbillings@gmail.com>
2025-04-20 22:30:33 +0100

    getting there

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,6 @@ module.exports = {
         name: "stockr-deno",
         script: "deno",
         args: "task start",
-        interpreter: "bash",
         interpreter_args: "run --allow-env --allow-read --allow-net",
         env: {
             PORT: 8000
